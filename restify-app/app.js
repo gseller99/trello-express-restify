ad953f40181c082698c5a513bae65b4eff943b43
gseller99 <33374369+gseller99@users.noreply.github.com>
2018-03-01 21:51:21 -0600

    2018 03 01

--- restify-app/app.js
+++ restify-app/app.js
@@ -6,58 +6,75 @@ server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
 
-let data = [{
-		"id": 1,
-		"subject": "hello",
-		"description": "hello world"
+
+//Data holding swimlane key: value: pairs
+let swimlaneArray = [{
+		"swimlaneHeader": "header1",
+		"swimlaneOrder": 0,
 	},
 	{
-		"id": 2,
-		"subject": "hello 2",
-		"description": "hello world 2"
+		"swimlaneHeader": "header2",
+		"swimlaneOrder": 0,
 	},
-	{
-		"id": 3,
-		"subject": "hello 3",
-		"description": "hello world 3"
-	}
 ];
 
-var Message = function(id, subject, description){
-	this.id = id;
-	this.subject = subject;
-	this.description = description;
+//Data holding cards key: value: pairs
+let cardArray = [{
+		"cardHeader": "header1",
+		"cardOrder": 0,
+	},
+	{
+		"cardHeader": "header2",
+		"cardOrder": 0,
+	},
+];
+
+//Constructor for swimlane data
+var Swimlane = function(swimlaneHeader, swimlaneOrder){
+	this.swimlaneHeader = swimlaneHeader;
+	this.swimlaneOrder = swimlaneOrder;
 }
 
+//Constructor for card data
+// var Swimlane = function(swimlaneHeader, swimlaneOrder){
+// 	this.swimlaneHeader = swimlaneHeader;
+// 	this.swimlaneOrder = swimlaneOrder;
+// }
 
-function getMessages(req, res, next) {
+function getSwimlanes(req, res, next) {
 	// Restify currently has a bug which doesn't allow you to set default headers
 	// These headers comply with CORS and allow us to serve our response to any origin
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "X-Requested-With");
 
-	//find the appropriate data
-	res.send(data);
+	//find the appropriate swimlaneArray
+	res.send(swimlaneArray);
 }
 
-function postMessage(req, res, next) {
+function postSwimlanes(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "X-Requested-With");
 	
 	console.log(req.body);
 
-	var message = new Message(req.body.id, req.body.subject, req.body.description);
+	var swimlane = new Swimlane(req.body.swimlaneHeader, req.body.swimlaneOrder);
 
-	data.push(message);
+	swimlaneArray.push(swimlane);
 
 	// save the new message to the collection
 
-	res.send(message);
+	res.send(swimlane);
 }
 
+
+
+
 // Set up our routes and start the server
-server.get('/messages', getMessages);
-server.post('/messages', postMessage);
+server.get('/swimlanes', getSwimlanes);
+server.post('/swimlanes', postSwimlanes);
+
+// server.get('/swimlanes', getSwimlanes);
+// server.post('/swimlanes', postSwimlanes);
 
 server.listen(8080, function() {
 	console.log('%s listening at %s', server.name, server.url);
