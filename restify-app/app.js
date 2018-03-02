616b224d7522a2e648f0306c60026dcc98412970
gseller99 <33374369+gseller99@users.noreply.github.com>
2018-03-02 02:03:25 -0600

    2018 03 02 1st

--- restify-app/app.js
+++ restify-app/app.js
@@ -36,11 +36,13 @@ var Swimlane = function(swimlaneHeader, swimlaneOrder){
 }
 
 //Constructor for card data
-// var Swimlane = function(swimlaneHeader, swimlaneOrder){
-// 	this.swimlaneHeader = swimlaneHeader;
-// 	this.swimlaneOrder = swimlaneOrder;
-// }
+var Card = function(cardHeader, cardOrder){
+	this.cardHeader = cardHeader;
+	this.cardOrder = cardOrder;
+}
 
+
+//Passing info for getting swimlanes
 function getSwimlanes(req, res, next) {
 	// Restify currently has a bug which doesn't allow you to set default headers
 	// These headers comply with CORS and allow us to serve our response to any origin
@@ -51,6 +53,18 @@ function getSwimlanes(req, res, next) {
 	res.send(swimlaneArray);
 }
 
+//Passing info for getting cards
+function getCards(req, res, next) {
+	// Restify currently has a bug which doesn't allow you to set default headers
+	// These headers comply with CORS and allow us to serve our response to any origin
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+
+	//find the appropriate swimlaneArray
+	res.send(cardArray);
+}
+
+//Passing info for posting swimlanes
 function postSwimlanes(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "X-Requested-With");
@@ -61,20 +75,36 @@ function postSwimlanes(req, res, next) {
 
 	swimlaneArray.push(swimlane);
 
-	// save the new message to the collection
+	// save the new swimlane to the collection
 
 	res.send(swimlane);
 }
 
+//Passing info for posting cards
+function postCards(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	
+	console.log(req.body);
+
+	var card = new Card(req.body.cardHeader, req.body.cardOrder);
+
+	cardArray.push(card);
+
+	// save the new card to the collection
+
+	res.send(card);
+}
 
 
-
-// Set up our routes and start the server
+// Set up our routes and start the server for swimlanes
 server.get('/swimlanes', getSwimlanes);
 server.post('/swimlanes', postSwimlanes);
 
-// server.get('/swimlanes', getSwimlanes);
-// server.post('/swimlanes', postSwimlanes);
+
+// Set up our routes and start the server for cards
+server.get('/cards', getCards);
+server.post('/cards', postCards);
 
 server.listen(8080, function() {
 	console.log('%s listening at %s', server.name, server.url);
